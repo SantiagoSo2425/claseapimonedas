aed9a2ee8ec6631585876472bf629dd42f103ec2
Santiago <Santiagosuarez1136374@correo.itm.edu.co>
2025-07-15 19:58:35 -0500

    Configure Spring Boot application for JPA and entity scanning

--- presentacion/src/main/java/ApiApplication.java
+++ presentacion/src/main/java/ApiApplication.java
@@ -2,8 +2,17 @@ package monedas.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+    "monedas.api.presentacion",
+    "monedas.api.aplicacion",
+    "monedas.api.infraestructura",
+    "monedas.api.core"
+})
+@EntityScan("monedas.api.dominio.entidades")
+@EnableJpaRepositories("monedas.api.infraestructura.repositorios")
 public class ApiApplication {
 
 	public static void main(String[] args) {
